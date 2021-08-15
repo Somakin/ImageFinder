f1b7fc8d338a58b832c547de90776dcf0ac98bf5
Somakin <joel.renggli@gmail.com>
2021-08-15 21:01:41 +0200

    a

---
+++ autoaccept/src/logic/AutoAction.java
@@ -0,0 +1,23 @@
+package logic;
+
+import java.awt.event.*;
+import java.awt.Robot;
+
+public class AutoAction {
+
+    public boolean klick(int[] xyPosition)throws Exception{
+
+        int x = xyPosition[0];
+        int y = xyPosition[1];
+        int mask = InputEvent.BUTTON1_DOWN_MASK;
+        Robot bot = new Robot();
+        if(x > 1){
+        bot.mouseMove(x, y);
+        bot.mousePress(mask);     
+        bot.mouseRelease(mask);
+        return true;
+        }
+        return false;
+    }
+    
+}
---
+++ autoaccept/src/logic/ImageFinder.java
@@ -0,0 +1,13 @@
+package logic;
+
+
+
+public class ImageFinder {
+
+    public static void main(String[] args) throws Exception {
+        Programm programm = new Programm();
+
+        programm.start();
+
+    } 
+}
