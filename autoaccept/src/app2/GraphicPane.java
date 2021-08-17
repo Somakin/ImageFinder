f7a4ca2631cbefe273d7ef6d44e1c53cda60f9f0
Somakin <joel.renggli@gmail.com>
2021-08-17 13:32:33 +0200

    a

--- autoaccept/src/app2/GraphicPane.java
+++ autoaccept/src/app2/GraphicPane.java
@@ -1,21 +1,45 @@
 package app2;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class GraphicPane extends Pane{
+public class GraphicPane extends StackPane{
 
-    Circle circle;
+    private Circle circle;
+    private Label label;
+    private TextField textfeld;
 
     public GraphicPane(){
         //init
         circle = new Circle(100, 100, 50, Color.AQUAMARINE);
+        label = new Label("Radius: ");
+        textfeld = new TextField();
+        textfeld.setEditable(true);
+
         //logic
+        
 
         //layout
-        getChildren().add(circle);
-        setStyle("-fx-background-color: WHEAT");
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(label,textfeld);
+        Pane circlePane = new Pane();
+        circlePane.getChildren().addAll(circle);
+        circlePane.setStyle("-fx-background-color: WHEAT");
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(hbox,circlePane);
+        VBox.setVgrow(circlePane, Priority.ALWAYS);
+
+        getChildren().addAll(vbox);
+
+        
+        
     }
     
 }
