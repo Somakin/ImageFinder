24d431b6acc07e32820c93c2a101ced645c77f88
Somakin <joel.renggli@gmail.com>
2021-08-17 14:50:12 +0200

    a

--- autoaccept/src/app/Starter.java
+++ autoaccept/src/app/Starter.java
@@ -2,32 +2,32 @@ package app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
-public class Starter extends Application{
+public class Starter extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
-
     @Override
     public void start(Stage stage) throws Exception {
-       
-        //init scene
+
+        // init scene
         MainPane hBox1 = new MainPane();
 
-        //layout
+        // layout
         stage.setTitle("League Q-Accepter");
         stage.setWidth(300);
-        Scene scene = new Scene(hBox1);
-        
 
-        //start
+        Scene scene = new Scene(hBox1);
+
+        // start
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
 
     }
-    
+
 }
