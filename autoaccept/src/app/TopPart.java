a6aabdfbbf0e1cd6add4faad1a726fbf0869c378
Somakin <joel.renggli@gmail.com>
2021-08-20 04:37:01 +0200

    a

--- autoaccept/src/app/TopPart.java
+++ autoaccept/src/app/TopPart.java
@@ -9,12 +9,7 @@ import javafx.scene.layout.HBox;
 import logic.AutoAction;
 import logic.ImageFinder;
 
-import java.awt.event.*;
 
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Clipboard;
 public class TopPart extends HBox {
 
     public Button button1;
@@ -24,7 +19,9 @@ public class TopPart extends HBox {
 
     public static volatile double trueX, trueY;
 
-    boolean gestoppt;
+    static volatile boolean gestoppt;
+    static volatile boolean ingame;
+    static volatile boolean inChampSelect;
 
     public TopPart() {
 
@@ -35,6 +32,7 @@ public class TopPart extends HBox {
         label = Beschriftungen.not_Searching;
         button3 = new Button("Exit");
         gestoppt = true;
+        ingame = false;
         trueX = 0;
         trueY = 0;
 
@@ -45,6 +43,7 @@ public class TopPart extends HBox {
             label.setText(Beschriftungen.searching.getText());
 
             try {
+                
                 search();
 
             } catch (Exception e) {
@@ -75,27 +74,49 @@ public class TopPart extends HBox {
             public void run() {
 
                 AutoAction aa = new AutoAction();
-                
 
                 ImageFinder acceptQ;
                 try {
-                    while ((!gestoppt && !(new ImageFinder("inChampSelect.png", 10).getImagefound()))
-                            && !(new ImageFinder("Moon.png", 10).getImagefound())
-                            || !(new ImageFinder("inChampSelect.png", 10).getImagefound())) {
-                        acceptQ = new ImageFinder("Moon.png", 10);
-                        if (acceptQ.getImagefound()) {
-                            aa.klick(acceptQ.getX(), acceptQ.getY());
-                        }
+                    while (!ingame) {
+                        if ((!gestoppt && !(new ImageFinder("inChampSelect.png", 10).getImagefound()))
+                                && !(new ImageFinder("Moon.png", 10).getImagefound())
+                                || !(new ImageFinder("inChampSelect.png", 10).getImagefound())) {
+                            inChampSelect = false;
+                            acceptQ = new ImageFinder("Moon.png", 10);
+                            if (acceptQ.getImagefound()) {
+                                aa.klick(acceptQ.getX(), acceptQ.getY());
+                                inChampSelect = true;
+                            }
 
+                        } else if ((new ImageFinder("inChampSelect.png", 10).getImagefound())) {
+
+                            // go to next stage: lock champion
+                            ImageFinder champselect = new ImageFinder("inChampSelect.png", 10);
+                            trueX = champselect.getX();
+                            trueY = champselect.getY();
+                            // write in chat
+                            aa.klick((int) trueX - 270, (int) trueY);
+                            String text = "I PICK: " + BottomPart.champLabel.getText();
+                            aa.writeAndEnter(text);
+                            // search champ
+                            aa.klick((int) trueX + 370, (int) trueY - 580);
+                            aa.writeAndEnter(BottomPart.champLabel.getText());
+                            // klick on champ
+                            Thread.sleep(500);
+                            aa.klick((int) trueX - 20, (int) trueY - 520);
+                            // lock
+                            Thread.sleep(500);
+                            aa.klick((int) trueX + 235, (int) trueY - 80);
+                            // backup if q gets doged
+                            while ((new ImageFinder("inChampSelect.png", 10).getImagefound())) {//TODO: muss anderes bild sein
+                                if(!(new ImageFinder("inChampSelect.png", 10).getImagefound()))//TODO: anders bild seon
+                                Thread.sleep(2000);
+                                if(!(new ImageFinder("inQ.png", 10).getImagefound())){
+                                    System.exit(0);
+                                }
+                            }
+                        }
                     }
-                    // go to next stage: lock champion
-                    ImageFinder champselect = new ImageFinder("inChampSelect.png", 10);
-                    trueX = champselect.getX();
-                    trueY = champselect.getY();
-                    aa.klick((int)trueX -270,(int)trueY);
-                    String text = "I PICK: " + BottomPart.champLabel.getText();
-                    aa.writeAndEnter(text);
-                    System.out.println(text);
 
                 } catch (Exception e) {
 
